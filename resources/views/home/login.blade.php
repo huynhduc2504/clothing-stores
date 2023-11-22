b313c2428a1413832d989ca879a0a9377737e8b7
trương17052001 <tran.quang.truong.17052001@gmail.com>
2023-11-22 20:50:02 +0700

    Login

--- resources/views/home/login.blade.php
+++ resources/views/home/login.blade.php
@@ -1,5 +1,29 @@
 @extends('layouts.layout')
 
 @section('content')
-    <h1>Login Page</h1>
+    <link rel="stylesheet" href="/css/login.css">
+    <div class="product-breadcrumb">
+            <h2>LOGIN</h2>
+            <a href="/">HOME / LOGIN</a>
+    </div>
+    <div class="modal-login">
+        <div class="modal_content">
+            <h1>LOGIN</h1>
+            <form class="login_box" action="/login" method="POST">
+                <div class="login_user">
+                    <span>Tài khoản hoặc email:</span>
+                    <input class="input_user" placeholder="Username or email"></input>
+                </div>
+                <div class="login_password">
+                    <span>Mật khẩu:</span>
+                    <input
+                        type="password"
+                        class="input_password"
+                        placeholder="Password"
+                    ></input>
+                </div>
+                <button class="login_button" type="submit">Login</button>
+            </form>
+        </div>
+    </div>
 @endsection
